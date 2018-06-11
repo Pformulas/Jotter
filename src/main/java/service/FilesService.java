a9639ddf801268122410e6ea3dea423e925eb1de
Mackyhuang <973151766@qq.com>
2018-06-12 00:13:29 +0800

    增加了文件模块的DAO层和service代码

--- src/main/java/service/FilesService.java
+++ src/main/java/service/FilesService.java
@@ -1,11 +1,14 @@
 package service;
 
 import common.ServerResponse;
+import entity.Files;
+import entity.NoteBook;
 import entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 文件操作业务层
@@ -25,4 +28,26 @@ public interface FilesService {
      */
     public ServerResponse<String> saveFile(CommonsMultipartFile file, String path, User user) throws IOException;
 
+
+    /**
+     * 查询这个用户的某个类型的文件
+     * @param files
+     * @return
+     */
+    public ServerResponse<List<Files>> listFile(Files files);
+
+    /**
+     * 删除指定url的文件
+     * @param url
+     * @return
+     */
+    public ServerResponse deleteFile(String url);
+
+    /**
+     * 根据url修改fileName
+     * 参数 url file
+     * @param files
+     * @return
+     */
+    public ServerResponse updateFilename(Files files);
 }
