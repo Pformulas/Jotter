259c3ea82227eed73bfb53d26a17ab30867013de
FishGoddess <1149062639@qq.com>
2018-06-11 19:52:37 +0800

    注册功能实现

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -11,56 +11,57 @@ const repoSwiper = new Swiper('#repoSwiper', {
     direction: 'vertical',
 });
 
-const SLIDE_SPEED = 800; // 切换速度，单位：ms
-
-// 清除原来的选择样式，并给指定元素加上样式
-function removeAndAddClass(ele) {
-    const navBtns = $(".navBtn li");
-    // 先清除原本留有的样式
-    for (let j = 0; j < navBtns.length; j++) {
-        $(navBtns[j]).removeClass("chosenLi");
-    }
-
-    // 再给这个被点击的加上
-    $(ele).addClass("chosenLi");
-}
-
-// 切换到登陆界面
-function slideToLoginPage() {
-    mainSwiper.slideTo(1, SLIDE_SPEED);
-    repoSwiper.slideTo(1, SLIDE_SPEED);
-
-    // 导航栏也要切换
-    removeAndAddClass(navBtns[1]);
-}
-
-// 切换到注册界面
-function slideToRegisterPage() {
-    mainSwiper.slideTo(1, SLIDE_SPEED);
-    repoSwiper.slideTo(0, SLIDE_SPEED);
-
-    // 导航栏也要切换
-    removeAndAddClass(navBtns[1]);
-}
-
-// 切换显示资料和修改资料的模块
-function switchShowOrUpdate() {
-    const detailDiv = $($(".detail")[0]);
-    const formDiv = $($(".form")[0]);
-
-    // 如果它原本是隐藏的，就显示出来，相反，就显示出来
-    if (detailDiv.hasClass("hidden")) {
-        detailDiv.removeClass("hidden");
-        formDiv.addClass("hidden");
-    } else {
-        formDiv.removeClass("hidden");
-        detailDiv.addClass("hidden");
-    }
-}
-
 $(function () {
     // 点击导航栏样式切换
     const navBtns = $(".navBtn li");
+
+    const SLIDE_SPEED = 800; // 切换速度，单位：ms
+
+    // 清除原来的选择样式，并给指定元素加上样式
+    function removeAndAddClass(ele) {
+        const navBtns = $(".navBtn li");
+        // 先清除原本留有的样式
+        for (let j = 0; j < navBtns.length; j++) {
+            $(navBtns[j]).removeClass("chosenLi");
+        }
+
+        // 再给这个被点击的加上
+        $(ele).addClass("chosenLi");
+    }
+
+    // 切换到登陆界面
+    function slideToLoginPage() {
+        mainSwiper.slideTo(1, SLIDE_SPEED);
+        repoSwiper.slideTo(1, SLIDE_SPEED);
+
+        // 导航栏也要切换
+        removeAndAddClass(navBtns[1]);
+    }
+
+    // 切换到注册界面
+    function slideToRegisterPage() {
+        mainSwiper.slideTo(1, SLIDE_SPEED);
+        repoSwiper.slideTo(0, SLIDE_SPEED);
+
+        // 导航栏也要切换
+        removeAndAddClass(navBtns[1]);
+    }
+
+    // 切换显示资料和修改资料的模块
+    function switchShowOrUpdate() {
+        const detailDiv = $($(".detail")[0]);
+        const formDiv = $($(".form")[0]);
+
+        // 如果它原本是隐藏的，就显示出来，相反，就显示出来
+        if (detailDiv.hasClass("hidden")) {
+            detailDiv.removeClass("hidden");
+            formDiv.addClass("hidden");
+        } else {
+            formDiv.removeClass("hidden");
+            detailDiv.addClass("hidden");
+        }
+    }
+
     for (let i = 0; i < navBtns.length; i++) {
         $(navBtns[i]).click(function () {
             // 清除原来的样式，然后给当前元素加上样式
@@ -91,4 +92,86 @@ $(function () {
     for (let i = 0; i < switchBtns.length; i++) {
         $(switchBtns[i]).click(switchShowOrUpdate);
     }
+
+    // 用户业务
+    // 给用户名输入框加样式，设置错误信息
+    function showError(msg) {
+        const error = $("#usernameError");
+        error.text(msg); // 设置错误信息
+        error.show(); // 显示错误信息
+
+        $("#inputUsername").addClass("inputError").focus(function () {
+            $(this).removeClass("inputError");
+            error.hide();
+        });
+    }
+
+    // 注册按钮绑定事件
+    $("#registerBtn").click(function () {
+        const form = $("#registerForm"); // 获取到表单信息
+        const pwd = $("#inputPwd"); // 获取密码输入框
+        const ensurePwd = $("#inputEnsurePwd"); // 获取用户输入的确认密码输入框
+
+        if (pwd.val() === ensurePwd.val()) {
+            // 密码和确认密码相等
+            if ($("#inputUsername").hasClass("inputError")) {
+                // 如果用户名验证有错误，则提示用户处理
+                alert("请处理错误再注册！");
+                return;
+            }
+
+            // 发送注册请求
+            $.ajax({
+                url: "/user/register.do",
+                type: "POST",
+                data: form.serialize(),
+                success: function (resp) {
+                    if (resp.status === 0) {
+                        alert(resp.msg); // 提示注册成功
+                        slideToLoginPage(); // 跳转到登陆界面
+                    } else {
+                        showError(resp.msg);
+                    }
+                },
+                error: function () {
+                    showError("网络错误！");
+                }
+            });
+        } else {
+            // 密码和确认密码不相等
+            $("#pwdError").show();
+            pwd.addClass("inputError");
+            ensurePwd.addClass("inputError");
+
+            // 当密码不匹配时，会触发红色警告，这个方法可以移除警告
+            function hideInputError() {
+                pwd.removeClass("inputError");
+                ensurePwd.removeClass("inputError");
+                $("#pwdError").hide();
+            }
+
+            // 当再次点击输入框的时候移除警告样式
+            pwd.focus(hideInputError);
+            ensurePwd.focus(hideInputError);
+        }
+    });
+
+    // 验证用户名，当用户名改变的时候
+    $("#inputUsername").on("change", function () {
+        // 发送请求去验证用户名
+        $.ajax({
+            url: "/user/check_username.do",
+            type: "POST",
+            data: $("#registerForm").serialize(),
+            success: function (resp) {
+                if (!(resp.status === 0)) {
+                    // 说明用户名不可用，加样式
+                    showError(resp.msg);
+                }
+            },
+            error: function () {
+                showError("网络错误。。。");
+            }
+        });
+    });
 });
