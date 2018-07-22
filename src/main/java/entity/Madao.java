7929934fed80dde64886288968b42d4686196143
Madao <790452097@qq.com>
2018-07-22 12:35:49 +0800

    新建madao类，用来测试github是否奔溃

---
+++ src/main/java/entity/Madao.java
@@ -0,0 +1,30 @@
+package entity;
+
+/**
+ * Created by rzh on 2018/07/22
+ */
+public class Madao
+{
+    private String name;
+    private int age;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
